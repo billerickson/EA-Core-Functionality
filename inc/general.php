0e19d1ae13915256eed01eaa1305eb349cce7c9f
Jon Brown <jbrown510@gmail.com>
2015-01-12 21:01:06 -1000

    Update general.php
    Fixed #27 - Please do give this a good look before merging.  I'm pretty sure it works, but then I haven't tested it in a long time.

--- inc/general.php
+++ inc/general.php
@@ -53,3 +53,23 @@ function ea_acf_options_page() {
     }
 }
 //add_action( 'init', 'ea_acf_options_page' );
+
+ /**
+ * Dont Update the Plugin
+ * If there is a plugin in the repo with the same name, this prevents WP from prompting an update.
+ *
+ * @since  1.0.0
+ * @author Jon Brown
+ * @param  array $r Existing request arguments
+ * @param  string $url Request URL
+ * @return array Amended request arguments
+ */
+function ea_dont_update_core_func_plugin( $r, $url ) {
+  if ( 0 !== strpos( $url, 'https://api.wordpress.org/plugins/update-check/1.1/' ) )
+    return $r; // Not a plugin update request. Bail immediately.
+    $plugins = json_decode( $r['body']['plugins'], true );
+    unset( $plugins['plugins'][plugin_basename( __FILE__ )] );
+    $r['body']['plugins'] = json_encode( $plugins );
+    return $r;
+ }
+add_filter( 'http_request_args', 'ea_dont_update_core_func_plugin', 5, 2 );
