507ce01f7978be17e0e6cace34d8203c2195ad41
Bill Erickson <bill.erickson@gmail.com>
2015-03-03 15:55:00 -0600

    move frontend functions to top of dev-tools file

--- inc/dev-tools.php
+++ inc/dev-tools.php
@@ -9,56 +9,6 @@
  */
 
 /**
- * Pretty Printing
- *
- * @since 1.0.0
- * @author Chris Bratlien
- * @param mixed $obj
- * @param string $label
- * @return null
- */
-function ea_pp( $obj, $label = '' ) {  
-	$data = json_encode( print_r( $obj,true ) );
-	?>
-	<style type="text/css">
-		#bsdLogger {
-		position: absolute;
-		top: 30px;
-		right: 0px;
-		border-left: 4px solid #bbb;
-		padding: 6px;
-		background: white;
-		color: #444;
-		z-index: 999;
-		font-size: 1.25em;
-		width: 400px;
-		height: 800px;
-		overflow: scroll;
-		}
-	</style>    
-	<script type="text/javascript">
-		var doStuff = function(){
-			var obj = <?php echo $data; ?>;
-			var logger = document.getElementById('bsdLogger');
-			if (!logger) {
-				logger = document.createElement('div');
-				logger.id = 'bsdLogger';
-				document.body.appendChild(logger);
-			}
-			////console.log(obj);
-			var pre = document.createElement('pre');
-			var h2 = document.createElement('h2');
-			pre.innerHTML = obj;
-			h2.innerHTML = '<?php echo addslashes($label); ?>';
-			logger.appendChild(h2);
-			logger.appendChild(pre);      
-		};
-		window.addEventListener ("DOMContentLoaded", doStuff, false);
-	</script>
-	<?php
-}
-
-/**
  * Shortcut function for get_post_meta();
  *
  * @since 1.2.0
@@ -107,6 +57,56 @@ function ea_first_term( $taxonomy = 'category', $field = 'name', $post_id = fals
 	
 	else
 		return $term;
+}
+
+/**
+ * Pretty Printing
+ *
+ * @since 1.0.0
+ * @author Chris Bratlien
+ * @param mixed $obj
+ * @param string $label
+ * @return null
+ */
+function ea_pp( $obj, $label = '' ) {  
+	$data = json_encode( print_r( $obj,true ) );
+	?>
+	<style type="text/css">
+		#bsdLogger {
+		position: absolute;
+		top: 30px;
+		right: 0px;
+		border-left: 4px solid #bbb;
+		padding: 6px;
+		background: white;
+		color: #444;
+		z-index: 999;
+		font-size: 1.25em;
+		width: 400px;
+		height: 800px;
+		overflow: scroll;
+		}
+	</style>    
+	<script type="text/javascript">
+		var doStuff = function(){
+			var obj = <?php echo $data; ?>;
+			var logger = document.getElementById('bsdLogger');
+			if (!logger) {
+				logger = document.createElement('div');
+				logger.id = 'bsdLogger';
+				document.body.appendChild(logger);
+			}
+			////console.log(obj);
+			var pre = document.createElement('pre');
+			var h2 = document.createElement('h2');
+			pre.innerHTML = obj;
+			h2.innerHTML = '<?php echo addslashes($label); ?>';
+			logger.appendChild(h2);
+			logger.appendChild(pre);      
+		};
+		window.addEventListener ("DOMContentLoaded", doStuff, false);
+	</script>
+	<?php
 }
 
 /**
