e22d3c8fc1970b41fc0d3ebec9edd5614fe338b2
Bill Erickson <bill.erickson@gmail.com>
2017-10-16 11:43:29 -0500

    setup carbon fields

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -10,3 +10,22 @@
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
+
+/**
+ * Load Carbon Fields
+ *
+ */
+function ea_load_carbon_fields() {
+    require_once( EA_DIR . '/vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+add_action( 'after_setup_theme', 'ea_load_carbon_fields' );
+
+/**
+ * Register Fields
+ *
+ */
+function ea_register_custom_fields() {
+
+}
+add_action( 'carbon_fields_register_fields', 'ea_register_custom_fields' );
